1f8da4b5b24ce69f26e6ac3d2009c84e8309a195
Nicholas Bastek <nicholas@example.com>
2026-08-31 09:19:18 +0700

    Add push scripts

---
+++ push.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d "c:\Users\Administrator\Desktop\04_Nicholas Bastek_IT5102"
+git push -u origin main
+git push -u origin staging
+pause
---
+++ push.ps1
@@ -0,0 +1,3 @@
+Set-Location "c:\Users\Administrator\Desktop\04_Nicholas Bastek_IT5102"
+& git push -u origin main
+& git push -u origin staging
